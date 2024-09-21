1860943642e78a56761293fbe0f73ae008d265ea
xsq <2420958662@qq.com>
2024-09-21 22:58:14 +0800

    Site updated: 2024-09-21 22:58:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/18/hello-world/"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/29c15ce3.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
