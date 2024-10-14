9d948d8e1da84528029788b33b5f2c8c25c5202b
xsq <2420958662@qq.com>
2024-10-14 10:04:45 +0800

    Site updated: 2024-10-14 10:04:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/f7dc1405.html","posts/29c15ce3.html","posts/7473ed9b.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/29c15ce3.html","posts/f7dc1405.html","posts/7473ed9b.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
